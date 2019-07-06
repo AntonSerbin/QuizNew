c5cbfceb5bef029b00593da28836658ef1419c29
a. serbin <antons.zn@gmail.com>
2019-07-06 12:50:57 +0300

    chgk

---
+++ 36 чгк 10/task.js
@@ -0,0 +1,50 @@
+//задания на уровень
+let taskName = "quiz36";
+const pauseAfterTask = 20; //sec пауза после уровня на подумать
+const tasks = [
+ 	task1 = {task:``,
+			secTask:60,
+			pict:"./images/01.jpg",
+			// audio:"./images/01.mp3",
+			secAnswer:15,
+//			formatAnswer:"",
+			answer:"Гамбургер. Применили к компании 'Макдоналдс' индекс Биг-Мака",
+			},
+ 	task2 = {task:``,
+			pict:"./images/02.jpg",
+			answer:"Кока-кола",
+			}
+			,
+ 	task3 = {task:``,
+			pict:"./images/03.jpg",
+			answer:"Прикусить язык. Один герой хотел съесть другого.",
+			},
+ 	task4 = {task:``,
+			pict:"./images/04.jpg",
+			answer:"Солнечные часы",
+			},
+ 	task5 = {task:``,
+			pict:"./images/05.jpg",
+			answer:"Каскадёр"
+			},
+ 	task6 = {task:``,
+			pict:"./images/06.jpg",
+			answer:"Лысых",
+			},
+ 	task7 = {task:``,
+			pict:"./images/07.jpg",
+			answer:"Букву Т в его фамилии",
+			},
+ 	task8 = {task:``,
+			pict:"./images/08.jpg",
+			answer:"Ставить на место",
+			},
+ 	task9 = {task:``,
+			pict:"./images/09.jpg",
+			answer:"Шум. Наушники защищали от шума города",
+			},
+ 	task10 = {task:``,
+			pict:"./images/10.jpg",
+			answer:"ДНК"
+			}
+]			
