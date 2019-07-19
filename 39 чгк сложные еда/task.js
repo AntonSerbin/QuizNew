0c393b9a6b6459097b03c1e920f9b57d6a21bde9
a. serbin <antons.zn@gmail.com>
2019-07-19 13:13:03 +0300

    added food CHGK

---
+++ 39 чгк сложные еда/task.js
@@ -0,0 +1,50 @@
+//задания на уровень
+let taskName = "quiz36";
+const pauseAfterTask = 20; //sec пауза после уровня на подумать
+const tasks = [
+ 	task1 = {task:``,
+			secTask:60,
+			pict:"./images/01.jpg",
+			// audio:"./images/01.mp3",
+			secAnswer:15,
+//			formatAnswer:"",
+			answer:"Лазанья",
+			},
+ 	task2 = {task:``,
+			pict:"./images/02.jpg",
+			answer:"Хинкали",
+			}
+			,
+ 	task3 = {task:``,
+			pict:"./images/03.jpg",
+			answer:"Маргарита (в честь неё повар назвал пиццу)",
+			},
+ 	task4 = {task:``,
+			pict:"./images/04.jpg",
+			answer:"Паэлья",
+			},
+ 	task5 = {task:``,
+			pict:"./images/05.jpg",
+			answer:"Эклер"
+			},
+ 	task6 = {task:``,
+			pict:"./images/06.jpg",
+			answer:"Сэндвич",
+			},
+ 	task7 = {task:``,
+			pict:"./images/07.jpg",
+			answer:"Завтрак, обед, ужин (парода собаки Бадуги)",
+			},
+ 	task8 = {task:``,
+			pict:"./images/08.jpg",
+			answer:"В школах сумо (Высококалорийное блюдо для набора массы).",
+			},
+ 	task9 = {task:``,
+			pict:"./images/09.jpg",
+			answer:"Игра Перстолов",
+			},
+ 	task10 = {task:``,
+			pict:"./images/10.jpg",
+			answer:"Гамбургер, картошка фри, другая еда МакДональдза"
+			}
+]			
