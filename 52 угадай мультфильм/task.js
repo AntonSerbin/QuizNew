c635f3b049b87744632d66eeee758b795fadd9fd
a. serbin <antons.zn@gmail.com>
2019-07-23 12:57:14 +0300

    added52-53

---
+++ 52 угадай мультфильм/task.js
@@ -0,0 +1,50 @@
+//задания на уровень
+let taskName = "";
+const pauseAfterTask = 30; //sec пауза после уровня на подумать
+const tasks = [
+ 	task1 = {task:``,
+			secTask:30,
+			pict:"./images/01.jpg",
+			// audio:"./images/01.mp3",
+			secAnswer:10,
+//			formatAnswer:"",
+			answer:"Фиксики",
+			},
+ 	task2 = {task:``,
+			pict:"./images/02.jpg",
+			answer:"Суперсемейка",
+			}
+			,
+ 	task3 = {task:``,
+			pict:"./images/03.jpg",
+			answer:"Ральф против интернета",
+			},
+ 	task4 = {task:``,
+			pict:"./images/04.jpg",
+			answer:"Волли",
+			},
+ 	task5 = {task:``,
+			pict:"./images/05.jpg",
+			answer:"Робокар Полли"
+			},
+ 	task6 = {task:``,
+			pict:"./images/06.jpg",
+			answer:"Зверополис",
+			},
+ 	task7 = {task:``,
+			pict:"./images/07.jpg",
+			answer:"Монстры на каникулах",
+			},
+ 	task8 = {task:``,
+			pict:"./images/08.jpg",
+			answer:"Король Лев",
+			},
+ 	task9 = {task:``,
+			pict:"./images/09.jpg",
+			answer:"Angry Birds",
+			},
+ 	task10 = {task:``,
+			pict:"./images/10.jpg",
+			answer:"Босс-молокосос"
+			}
+]			
